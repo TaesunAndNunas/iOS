e785790226ec083c854a8f82571c4f34104182ad
yunseo <yunseo@nhnnext.org>
2015-04-13 16:55:01 +0900

    git merge&fetch를 위한 작업2

---
+++ ImFine_storyboard/ImFine_storyboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImFine_storyboard
+//
+//  Created by yunseo shin on 2015. 3. 29..
+//  Copyright (c) 2015년 yunseo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ImFine_storyboard/ImFine_storyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImFine_storyboard
+//
+//  Created by yunseo shin on 2015. 3. 29..
+//  Copyright (c) 2015년 yunseo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
